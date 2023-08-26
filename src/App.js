1b1248b33a1e66aebed8cdf570e05894dead29e2
Andrevictor20 <69045848+Andrevictor20@users.noreply.github.com>
2023-08-26 14:48:35 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -95,33 +95,33 @@ const App = () => {
         <Input value= {currentNumber}/>
         <Row>
           <Button label="AC" onClick={handleOnCLear}/>
-          <Button label="X" />
           <Button label="/" onClick={handleDivNumbers}/>
           <Button label="x" onClick={handleMultNumbers} />
+          <Button label="-" onClick={handleMinusNumbers} />
         </Row>
         <Row>
           <Button label="7" onClick={()=>handleAddNumber('7')}/>
           <Button label="8" onClick={()=>handleAddNumber('8')}/>
           <Button label="9" onClick={()=>handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers} />
+          <Button label="+" onClick={handleSumNumbers}/>
+        
         </Row>
     
         <Row>
           <Button label="4" onClick={()=>handleAddNumber('4')}/>
           <Button label="5" onClick={()=>handleAddNumber('5')}/>
           <Button label="6" onClick={()=>handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="=" onClick={handleEquals}/>
+          
         </Row>
         <Row>
           <Button label="1" onClick={()=>handleAddNumber('1')}/>
           <Button label="2" onClick={()=>handleAddNumber('2')}/>
           <Button label="3" onClick={()=>handleAddNumber('3')}/>
-          <Button label="=" onClick={handleEquals}/>
-        </Row>
-        <Row>
           <Button label="0" onClick={()=>handleAddNumber('0')}/>
-         
+          
         </Row>
+       
        
       </Content>
     </Container>
